81acc664a9f6f4493708599574cb9221d4b18f67
Nicholas Buhro <n.buhro@gmail.com>
2016-03-14 18:22:32 +0300

    Solution is created (with base and test projects).

---
+++ NickBuhro.NumToWords.Tests/RussianInWordsFormatterTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NickBuhro.NumToWords.Tests
+{
+    [TestClass]
+    public class RussianInWordsFormatterTests
+    {
+        [TestMethod]
+        public void Int32FormatTest()
+        {
+            var testCases = new []
+            {
+                new Tuple<int, string>(1, "один"),
+                new Tuple<int, string>(0, "ноль"),
+                new Tuple<int, string>(-5, "минус пять"),
+                new Tuple<int, string>(100, "сто")
+            };
+
+            var formatter = new RussianInWordsFormatter();
+
+            foreach (var testCase in testCases)
+            {
+                var actual = testCase.Item1.ToString(formatter);
+                Assert.AreEqual(testCase.Item2, actual);
+            }
+        }
+    }
+}
